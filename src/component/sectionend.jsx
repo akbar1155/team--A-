189984835f13063ea50600c50cb910c8327d50d0
Akbar <isroilovakbar49@gmail.com>
2023-03-20 06:48:38 +0500

    djchsb

---
+++ src/component/sectionend.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import "./sectionend.scss"
+import "../image/Ellipse 1951.png"
+const Sectionend = () => {
+    return (
+        <div className='div1'>
+            <div className='div1__div1'>
+                <h1 className='div1__div1__h1'>Sizni bir finjon qahva ustida
+                    loyihangizni batafsil muhokama
+                    qilishga mamnuniyat ila taklif
+                    qilamiz.</h1>
+                <div className='div1__div1__inputs'>
+                    <input type="text"
+                        className='input1' placeholder='Ismingiz' />
+                    <input type="text"
+                        className='input1' placeholder='Telefon raqamingiz' />
+                    <input type="text"
+                        className='input1' placeholder='Goyangiz xaqida qisqacha (Muhim emas)' />
+
+                </div>
+                <h2 className='div1__div1__h2'>Qanday xizmatlardan foydalanishni xoxlaysiz?</h2>
+                <div className='df'>
+                    <ul className="div1__div1__ul">
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+
+
+                    </ul>
+                    <ul className="div1__div1__ul">
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                        <li className="div1__div1__ul__li">
+                            <input type="checkbox"
+                                className="div1__div1__ul__li__input1" />
+                            <p
+
+                                className="div1__div1__ul__li__p1">Backend</p>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <div className='div1__div2'>
+                <div className='div1__div2__div1'>
+                    <h3  className='div1__div2__div1__h3'>NIMANI MA'QUL KO'RASIZ?</h3>
+                    <ul  className='div1__div2__div1__ul'>
+                        <li className='div1__div2__div1__ul__li'>
+
+
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default Sectionend;
